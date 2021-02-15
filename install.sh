403acaa7479dcd2d183218a724660aea3b77f5df
krishpranav <krisna.pranav@gmail.com>
2021-02-15 17:07:15 +0530

    added helper file to install the dependencies

---
+++ install.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt update
+apt install lolcat
+apt install python3
+apt install wget
+apt install curl
+pip3 install lolcat
+echo "Completed Downloading requirements"
+sleep 2
+echo "Now Run bash cookie-steal.sh"
